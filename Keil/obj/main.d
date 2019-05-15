7744529adf9cf2aa91ec9c550372d663fa28ddd0
Evan Reeves <evanreeves12@gmail.com>
2019-05-14 22:52:10 -0700

    initial commit

---
+++ Keil/obj/main.d
@@ -0,0 +1,2 @@
+.\obj\main.o: src\main.c
+.\obj\main.o: .\inc\main.h
---
+++ Keil/src/main.c
@@ -0,0 +1,13 @@
+/**
+* @author Evan Reeves
+* @ date 2019-05-14
+*/
+
+#include "main.h"
+
+int main(void)
+{
+    
+	while(1) {
+	}
+}
