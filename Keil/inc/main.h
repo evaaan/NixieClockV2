04d453424a023f8fbabc198db3591dd25a94b6c6
Evan Reeves <evanreeves12@gmail.com>
2019-05-16 22:37:44 -0700

    working commit

--- Keil/inc/main.h
+++ Keil/inc/main.h
@@ -3,5 +3,16 @@
 */
 
 #include "stm32f4xx_hal.h"
-void LED_Init(void);
+
+typedef struct {
+  uint16_t hours;
+  uint16_t minutes;
+  uint16_t seconds;
+}time;
+
 void GPIO_Init(void);
+void inc_seconds(void);
+void inc_minutes(void);
+void inc_hours(void);
+void update(void);
+uint16_t int2bcd(uint16_t);
--- Keil/src/main.c
+++ Keil/src/main.c
@@ -8,26 +8,71 @@
 #include "stm32f4xx.h"
 
 uint16_t idx;
+volatile time clock;
+
 GPIO_InitTypeDef GPIO_InitStruct;
 
 int main(void) {
   /* Initialize HAL Library. */
   HAL_Init();
   
+  /* Initialize GPIOs */
   GPIO_Init();
   
+  clock.hours = 0;
+  clock.minutes = 0;
+  clock.seconds = 0;
+  
 	while(1) {
-    // HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_10);
-    // HAL_Delay(1000);
-    
-    for (idx = 0; idx < 10; idx++) {
-      HAL_GPIO_WritePin(GPIOD, GPIO_PIN_8, idx & 0x01);
-      HAL_GPIO_WritePin(GPIOD, GPIO_PIN_9, idx & 0x02);
-      HAL_GPIO_WritePin(GPIOD, GPIO_PIN_10, idx & 0x04);
-      HAL_GPIO_WritePin(GPIOD, GPIO_PIN_11, idx & 0x08);
-      HAL_Delay(1000);
-    }
+    update();
+    HAL_Delay(1000);
+    inc_seconds();
 	}
+}
+
+/* Update Nixie Tubes */
+void update(void) {
+  uint16_t sec, sec_ones, sec_tens;
+  sec = int2bcd(clock.seconds);
+  sec_ones = sec & 0x0F;
+  sec_tens = (sec & 0xF0) >> 4;
+  
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_8, sec_tens & 0x01);
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_9, sec_tens & 0x02);
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_10, sec_tens & 0x04);
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_11, sec_tens & 0x08);
+}
+
+/* Convert integer (0 to 0x63) to binary-coded decimal (0x00 to 0x99)*/
+uint16_t int2bcd(uint16_t val) {
+  return ((val / 10) << 4) | (val % 10);
+}
+
+/* Time update methods */
+void inc_seconds(void) {
+  if (clock.seconds == 59) {
+    clock.seconds = 0;
+    inc_minutes();
+  } else {
+    clock.seconds += 1;
+  }
+}
+
+void inc_minutes(void) {
+  if (clock.minutes == 59) {
+    clock.minutes = 0;
+    inc_hours();
+  } else {
+    clock.minutes += 1;
+  }
+}
+
+void inc_hours(void) {
+  if (clock.hours == 12) {
+    clock.hours = 1;
+  } else {
+    clock.hours += 1;
+  }
 }
 
 void GPIO_Init(void) {
